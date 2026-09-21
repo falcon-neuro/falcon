0330d38d79520a3f9f108b288192a73e14abdd7d
ben <benfrost42@icloud.com>
2026-09-21 12:23:02 +0200

    wip

--- scripts/make.sh
+++ scripts/make.sh
@@ -3,5 +3,4 @@ set -e
 cd build
 make -j$(nproc 2>/dev/null)
 mkdir -p debug
-cp --remove-destination falcon/falcon debug/
-cp --remove-destination falcon/tools0/nlxtestbench/nlxtestbench debug/ || true
+cp --remove-destination falcon debug/
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Falcon initialized." << std::endl;
+    return 0;
+}
